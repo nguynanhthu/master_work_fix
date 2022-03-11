60822431fc517dddf40ceeeeb5a4223c01954cba
nguynanhthu <2054052066thu@ou.edu.vn>
2022-03-11 15:07:12 +0700

    upload source

---
+++ main.js
@@ -0,0 +1,16 @@
+// var projectBodyList = document.getElementById("project-body-list");
+// var listNavItem = projectBodyList.getElementsByClassName("nav-item");
+// // for (let index = 0; index < listNavItem.length; index++) {
+// //     var e = listNavItem[index];
+// //     e.addEventListener("click", ()=>{
+// //         // e.classList.add("active")
+// //         for (let index = 0; index < listNavItem.length; index++){
+// //             var element = listNavItem[index];
+// //             if(element.classList.contains("nav-item-active"))
+// //                 element.classList.remove("nav-item-active")
+// //         }
+// //         e.classList.add("nav-item-active")
+// //     })
+// // }
+
+// khoa301000@gmail.com
